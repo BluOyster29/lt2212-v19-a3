c53dd574138ed61712cb5dbadb2921db0aa3bd34
Robert <gusroberth@student.gu.se>
2019-03-12 12:48:52 +0100

    'more fancy formatting'

---
+++ export_stats.py
@@ -0,0 +1,42 @@
+import subprocess,os,pandas as pd, numpy as np
+import matplotlib.pyplot as plt
+import tabulate as t
+
+def generate_stats():
+    statistics = []
+    for subdir, dirs, files in os.walk('results'):
+        for x in files:
+            if '.md' in str(os.path.join(subdir, x)):
+                statistics.append(subdir + '/' + x)
+             
+    stat_model = []
+    columns = ['file_name','lines', 'ngram','pos','accuracy','perplexity']
+    
+    for stat in sorted(statistics):
+        with open(stat, 'r') as data:
+            lines = data.readlines()
+            total_lines = lines[1].split(' ')[4].split('/')[1].split('_')[0]
+            train_test = lines[1].split(' ')[4].split('/')[1]
+            accuracy = float(lines[7].split(' ')[1][:-6])
+            perplexity = float(lines[9].split(' ')[1][:-6])
+            if 'pos' in train_test:
+                pos = True
+            else:
+                pos = False
+            ngram =  lines[5].split(' ')[1]
+            stat_model.append([train_test,total_lines,ngram,pos,accuracy,perplexity])
+
+    df = pd.DataFrame(stat_model, columns=columns)
+    return df
+
+if __name__ == "__main__":
+
+    df = generate_stats()
+    f = open('statistics.md', 'w+')
+    f.write(t.tabulate(df,tablefmt="markdown", headers="keys"))
+    f.close
+    
+
+
+
+
---
+++ statistics.md
@@ -0,0 +1,7 @@
+    file_name        lines  ngram    pos      accuracy    perplexity
+--  -------------  -------  -------  -----  ----------  ------------
+ 0  100_lines          100  2-gram   False    0.288073       55.4609
+ 1  100_lines_n3       100  3-gram   False    0.246479       65.5333
+ 2  100_lines_pos      100  2-gram   True     0.446927       69.1123
+ 3  200_lines          200  2-gram   False    0.286672       97.3902
+ 4  200_lines_pos      200  2-gram   True     0.409574      128.132
